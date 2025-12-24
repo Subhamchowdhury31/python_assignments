a93ad964d918d108f8f737cc0703685c081050c8
Shuvo's <subhamchowdhury@Shuvo-2.local>
2025-12-24 15:28:53 +0530

    lists question

---
+++ lists.py
@@ -0,0 +1,35 @@
+#-------------List duplicate----------
+
+# list1=[1,1,2,2,3,3,4,5]
+# newList=[]
+# for i in list1:
+#     if i not in newList:
+#         newList.append(i)
+# print(newList)
+
+#-------------list comprehension--------
+# list1=[1,2,3,4,5,6,7,8]
+# newList=[]
+
+# for i in list1:
+#     if i%2==0:
+#         newList.append(i)
+# print(newList)
+
+#-----------second largest element in list----------
+# list1=[1,2,3,4,5,6,7,8]
+# largest=max(list1)
+# list1.remove(largest)
+# secondlargest=max(list1)
+# print("the second largest element is ",secondlargest)
+
+
+#----------sum of the lists---------
+lst = [[1,2],[3,4],[5,6]]
+total = 0
+lst2=[]
+for sub in lst:
+    for i in sub:
+        total += i
+    lst2.append(total)
+print(lst2)
