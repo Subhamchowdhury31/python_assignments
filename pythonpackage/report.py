b97544c298dbd5f2c86b148baf9a4ff6b2ca66fa
Shuvo's <subhamchowdhury@Shuvo-2.local>
2025-12-26 13:31:54 +0530

    modified

--- pythonpackage/report.py
+++ pythonpackage/report.py
@@ -2,6 +2,7 @@ from student import getStudent
 from attendence import attends
 from marks import mark
 from fees import getFee
+from utils import percentage
 
 class Report:
 
@@ -11,6 +12,10 @@ class Report:
         if student is None:
             print("Student not found")
             return
+        
+        marks_dict = mark(sid)
+        total_marks = sum(marks_dict.values()) if marks_dict else 0
+        percent = percentage(total_marks, 300) 
 
         print("\n========== STUDENT REPORT ==========")
         print("Student ID :", sid)
@@ -19,5 +24,6 @@ class Report:
         print("Attendance :", attends(sid), "days")
         print("Marks      :", mark(sid))
         print("Fees Paid  :", getFee(sid))
+        print("percentage :",percent,"%")
         print("===================================\n")
 
